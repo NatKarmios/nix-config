c894cab1d169d0ebdd1cc3da2c1ead116d250212
Nat Karmios <nat@karmios.com>
2025-07-07 12:07:37 +0100

    Some Nixvim config

--- home/nat/common/core/nixvim/default.nix
+++ home/nat/common/core/nixvim/default.nix
@@ -1,6 +1,5 @@
 {
   inputs,
-  pkgs,
   ...
 }:
 {
@@ -12,6 +11,33 @@
     enable = true;
     enableMan = true;
     colorschemes.catppuccin.enable = true;
+
+    globals = {
+      have_nerd_font.__raw = ''
+        vim.env.TERM ~= "linux"
+      '';
+    };
+
+    opts = {
+      number = true;
+      list = true;
+      listchars = {
+        tab = "» ";
+        trail = "·";
+        nbsp = "␣";
+      };
+    };
+
+    plugins = {
+      numbertoggle.enable = true;
+      lsp = {
+        enable = true;
+        servers = {
+          nixd.enable = true;
+          rust_analyzer.enable = true;
+        };
+      };
+    };
   };
 }
 
