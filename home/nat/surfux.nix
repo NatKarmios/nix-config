1cf326d23db19b6632f6d52fe10b243c0e054c0c
Nat Karmios <nat@karmios.com>
2025-07-07 12:04:56 +0100

    Add Hyprland backup, add waybar

--- home/nat/surfux.nix
+++ home/nat/surfux.nix
@@ -1,8 +1,11 @@
-{ ... }:
+{ pkgs, ... }:
 {
   imports = [
     common/core
+    common/optional/hyprland
     common/optional/niri
+    common/optional/waybar.nix
+  ];
 
   home.packages = with pkgs; [
     obsidian
--- hosts/common/optional/desktop/hyprland.nix
+++ hosts/common/optional/desktop/hyprland.nix
@@ -1,8 +1,32 @@
-{ pkgs,  ... }:
-{
-  programs.hyprland = {
-    enable = true;
-    withUWSM = true;
+{ inputs, pkgs, ... }:
+let
+  path = "/run/current-system/sw/bin";
+  hyprland-uwsm = pkgs.writeShellScriptBin "hyprland-uwsm" ''
+    exec ${path}/uwsm start -S -F ${path}/hyprland
+  '';
+  desktop-entry = pkgs.writeTextFile {
+    name = "hyprland-uswm";
+    text = ''
+      [Desktop Entry]
+      Name=Hyprland (UWSM)
+      Comment=Dynamic tiling Wayland compositor that doesn't sacrifice on its looks (with UWSM, manual desktop entry)
+      Exec=${path}/hyprland-uwsm
+    '';
+    destination = "/share/wayland-sessions/hyprland-uwsm.desktop";
+    derivationArgs = {
+      passthru.providedSessions = [ "hyprland-uwsm" ];
+    };
   };
+in
+{
+  programs.hyprland.enable = true;
+  programs.hyprlock.enable = true;
+  services.hypridle.enable = true;
+
+  environment.systemPackages = [ hyprland-uwsm ];
+
+  services.displayManager.sessionPackages = [
+    desktop-entry
+  ];
 }
 
