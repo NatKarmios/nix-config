3179ca68968a5ceda74330a92ded494666c60624
Nat Karmios <nat@karmios.com>
2025-07-08 15:48:33 +0100

    Further Nix config

--- home/nat/common/core/nixvim/default.nix
+++ home/nat/common/core/nixvim/default.nix
@@ -1,10 +1,11 @@
-{
-  inputs,
-  ...
-}:
+{ lib, inputs, ... }:
+with lib.custom.nixvim;
 {
   imports = [
     inputs.nixvim.homeManagerModules.nixvim
+    ./plugins/lsp.nix
+    ./plugins/telescope.nix
+    ./plugins/which-key.nix
   ];
 
   programs.nixvim = {
@@ -13,31 +14,110 @@
     colorschemes.catppuccin.enable = true;
 
     globals = {
-      have_nerd_font.__raw = ''
+      mapleader = " ";
+      maplocalleader = " ";
+      have_nerd_font = raw ''
         vim.env.TERM ~= "linux"
       '';
     };
 
     opts = {
-      number = true;
+      number = true;  # Line numbers
+      mouse = "a";  # Enable mouse
+      showmode = false;  # Hide mode; statusline will show it
+      breakindent = true;
+      undofile = true;  # Undo history
+
+      # Ignore case in searches UNLESS \C or capital letters in search term
+      ignorecase = true;
+      smartcase = true;
+
+      signcolumn = "yes";
+      updatetime = 250;  # Decrease update time
+      timeoutlen = 300;  # Decrese mapped sequence wait time
+
+      # Set how new splits should be opened
+      splitright = true;
+      splitbelow = true;
+
+      # Show whitespace characters
       list = true;
       listchars = {
         tab = "» ";
         trail = "·";
         nbsp = "␣";
       };
+
+      inccommand = "split";  # Substitutions live preview
+      cursorline = true;  # Show which line the cursor is on
+      scrolloff = 10;  # Min lines to keep above and below cursor
+
+      # Fold settings; copped from `nvim-ufo` README
+      foldcolumn = "1";
+      fillchars = "eob: ,fold: ,foldopen:,foldsep: ,foldclose:";
+      foldlevel = 99;
+      foldlevelstart = 99;
+      foldenable = true;
     };
 
     plugins = {
-      numbertoggle.enable = true;
-      lsp = {
+      cmp = {
         enable = true;
-        servers = {
-          nixd.enable = true;
-          rust_analyzer.enable = true;
+        autoEnableSources = true;
+        settings = {
+          sources = [
+            { name = "nvim_lsp"; }
+            { name = "luasnip"; }
+            { name = "path"; }
+            { name = "nvim_lsp_signature_help"; }
+          ];
         };
       };
+      gitsigns.enable = true;  # Add git signs to the gutter
+      numbertoggle.enable = true;  # Auto-toggle relative/absolute nums based on mode
+      sleuth.enable = true;  # Configure tabs and indents automatically
     };
+
+    keymaps = with bind-helpers; lib.flatten [
+      (n "<Esc>" (cmd "nohlsearch")) # Clear search highlights
+      (t' "<Esc><Esc>" ''<C-\\><C-n>'' "Exit terminal mode")
+
+      # Disable arrows in normal mode
+      (nv "<left>" (cmd ''echo "Use h to move!"''))
+      (nv "<right>" (cmd ''echo "Use l to move!"''))
+      (nv "<up>" (cmd ''echo "Use j to move!"''))
+      (nv "<down>" (cmd ''echo "Use k to move!"''))
+
+      # Nicer split navigation
+      (n' "<C-h>" "<C-w><C-h>" "Move focus to the left window")
+      (n' "<C-l>" "<C-w><C-h>" "Move focus to the right window")
+      (n' "<C-j>" "<C-w><C-h>" "Move focus to the lower window")
+      (n' "<C-k>" "<C-w><C-h>" "Move focus to the upper window")
+
+      # My custom bindings; inspired by what I'm used to from DOOM Emacs
+      (n' "<leader><Tab>" (cmd "tabn") "Next tab")
+      (n' "<leader><S-Tab>" (cmd "tabN") "Previous tab")
+      (n' "<leader>bd" (cmd "bd") "[B]uffer [D]elete")
+      (n' "<leader>bc" (cmd "bc") "[B]uffer [C]reate")
+      (n' "<leader>bn" (cmd "bn") "[B]uffer [N]ext")
+      (n' "<leader>bN" (cmd "bN") "[B]uffer Previous")
+      (n' "gb" "<C-o>" "[G]o [B]ack")
+      (n' "gB" "<C-i>" "[G]o Forward")
+      (i "jj" "<Esc>")
+    ];
+
+    autoGroups = {
+      nat-highlight-yank.clear = true;
+    };
+
+    autoCmd = [
+      {
+        event = "TextYankPost";
+        desc = "Highlight when yanking text";
+        group = "nat-highlight-yank";
+        callback = raw "function() vim.highlight.on_yank() end";
+      }
+    ];
   };
 }
 
--- lib/default.nix
+++ lib/default.nix
@@ -19,5 +19,39 @@
     (name: value: lib.nameValuePair (prefix + name) value)
     attrs;
   mod = a: b: a - ((builtins.div a b) * b);
+  nixvim = rec {
+    raw = s: { __raw = s; };
+
+    # Quick bind functions
+    bind-helpers = rec {
+      bind'' = mode: key: action: options: {
+        inherit mode key action options;
+      };
+      bind' = mode: key: action: desc: bind'' mode key action { inherit desc; };
+      bind = mode: key: action: bind'' mode key action {};
+
+      n = bind "n";
+      n' = bind' "n";
+      i = bind "i";
+      t' = bind' "t";
+      nv = bind ["n" "v"];
+      nx' = bind' ["n" "x"];
+
+      cmd = s: "<cmd>${s}<CR>";
+    };
+
+    toAttrKeymap = bs:
+      let attrs = map ({ key, ... }@b:
+        let value = lib.removeAttrs b [ "key" ]; in
+        { name = key; inherit value; })
+        (lib.flatten bs);
+      in
+      lib.listToAttrs attrs;
+
+    # Bind transformers
+    mapBinds = f: bs: map f (lib.flatten bs);
+    prependKey = p: mapBinds (b: b // { key = p + b.key; });
+    prependRawAction = p: mapBinds (b: b // { action = (raw (p + b.action)); });
+  };
 }
 
