eb5f3ae6695480c186022a6ecf8b9216cc48a5b3
Nat Karmios <nat@karmios.com>
2025-07-29 14:39:08 +0100

    Add ssh key to agent when signing commits

--- home/nat/common/optional/git.nix
+++ home/nat/common/optional/git.nix
@@ -1,4 +1,15 @@
-{ lib, config, ... }:
+{ lib, pkgs, config, ... }:
+let
+  # https://www.reddit.com/r/git/comments/1coropv/comment/l3mwfso/
+  git-ssh-sign = pkgs.writeShellScript "git-ssh-sign" ''
+    #!/bin/sh
+    while getopts Y:n:f: opt; do case $opt in 
+    f) ssh-add -T "$OPTARG" 2>&- || ssh-add "$OPTARG" ;;
+    esac; done
+
+    exec ssh-keygen "$@"
+  '';
+in
 {
   programs.git = {
     enable = true;
@@ -6,6 +17,7 @@
       format = "ssh";
       key = "${config.home.homeDirectory}/.ssh/id_ed25519";
       signByDefault = true;
+      signer = "${git-ssh-sign}";
     };
     userName = lib.mkDefault config.hostSpec.userFullName;
     userEmail = lib.mkDefault config.hostSpec.email;
