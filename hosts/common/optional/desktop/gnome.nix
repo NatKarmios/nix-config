a8e0deb9898000c9dc3417dc32256e5dd0c8da17
Nat Karmios <nat@karmios.com>
2026-05-28 11:01:59 +0100

    Slim down GNOME install

--- hosts/common/optional/desktop/gnome.nix
+++ hosts/common/optional/desktop/gnome.nix
@@ -1,9 +1,24 @@
+{ pkgs, ... }:
 {
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
   services.desktopManager.gnome.enable = true;
+
+  services.gnome = {
+    core-apps.enable = false;
+    core-developer-tools.enable = false;
+    games.enable = false;
+  };
+  environment.gnome.excludePackages = with pkgs; [
+    gnome-tour
+    gnome-user-docs
+  ];
+
+  environment.systemPackages = with pkgs; [
+    nautilus
+  ];
 
   # Configure keymap in X11
   services.xserver.xkb = {
