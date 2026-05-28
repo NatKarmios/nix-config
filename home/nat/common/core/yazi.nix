e4be129cb89f1e99e66e0a57505edfcc10ca90c7
Nat Karmios <nat@karmios.com>
2026-05-28 11:04:00 +0100

    Add ripdrag, integrate with Yazi

--- home/nat/common/core/yazi.nix
+++ home/nat/common/core/yazi.nix
@@ -1,10 +1,28 @@
 #
 # Terminal file browser
 #
+{ pkgs, ... }:
 {
   programs.yazi = {
     enable = true;
     enableZshIntegration = true;
     shellWrapperName = "y";
+    plugins = {
+      drag = (
+        pkgs.fetchFromGitHub {
+          owner = "Joao-Queiroga";
+          repo = "drag.yazi";
+          rev = "3dff129c52b30d8c08015e6f4ef8f2c07b299d4b";
+          hash = "sha256-nmFlh+zW3aOU+YjbfrAWQ7A6FlGaTDnq2N2gOZ5yzzc=";
+        }
+      );
+    };
+    keymap = {
+      mgr.prepend_keymap = [
+        { run = "plugin drag"; on = [ "<C-d>" ]; desc = "Drag files"; }
+      ];
+    };
   };
+
+  home.packages = with pkgs; [ ripdrag ];
 }
