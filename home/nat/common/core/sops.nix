944c27ef6ab93adc7c4ad0561e9e931226bcf243
Nat Karmios <nat@karmios.com>
2026-08-15 00:08:04 +0100

    Use SSH key for user-level sops

--- home/nat/common/core/sops.nix
+++ home/nat/common/core/sops.nix
@@ -8,7 +8,9 @@ in
   ];
 
   sops = {
-    age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
+    age.sshKeyPaths = [
+      "${config.home.homeDirectory}/.ssh/id_ed25519"
+    ];
 
     defaultSopsFile = "${secretsPath}/secrets.yaml";
     validateSopsFiles = false;
